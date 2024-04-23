4237e3a4dfc8cac0c169d515c7c1ae35ec529f8a
Hardikmaind <maindhardik@gmail.com>
2024-04-23 20:52:50 +0530

    code added

--- req.py
+++ req.py
@@ -8,3 +8,18 @@ data=res.json()
 if 'content' in data:
     code=base64.b64decode(data['content']).decode('utf-8')
     print(code)
+
+
+
+
+
+
+
+
+
+
+#below is the another method no need to decode
+
+
+import requests
+print(requests.get("https://raw.githubusercontent.com/Hardikmaind/prac8/main/dl/ass1.txt").text)
