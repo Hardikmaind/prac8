dcb58ecf6d65611ae719f5520fe673cdcf1c9ed1
Hardikmaind <maindhardik@gmail.com>
2024-04-23 19:47:02 +0530

    code added

--- req.py
+++ req.py
@@ -1,7 +1,7 @@
 
 import requests
 import base64
-endpoint="https://api.github.com/repos/Hardikmaind/prac8/contents/demo.py"
+endpoint="https://api.github.com/repos/Hardikmaind/prac8/contents/dl/ass1.txt"
 res=requests.get(endpoint)
 data=res.json()
 
